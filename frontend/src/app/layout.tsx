91328c2641af8cb199f6edba57e6b01b46bc14bb
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2026-05-04 23:40:09 -0300

    chore: configura ambiente inicial com dependências e estrutura base do frontend e backend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Diário de Classe",
+  description: "Aplicação web para professores registrarem o desempenho mensal de alunos de forma digital. Permite criar turmas, adicionar alunos e gerar relatórios em PDF por turma. Focada em simplicidade e usabilidade, substitui a caderneta física com uma solução prática e organizada.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-br"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
