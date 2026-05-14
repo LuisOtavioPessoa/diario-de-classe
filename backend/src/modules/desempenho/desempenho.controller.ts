864a12d7ac874b20f2f0246b1ec138d321038e82
Luís Otávio <143001279+LuisOtavioPessoa@users.noreply.github.com>
2026-05-14 20:45:41 -0300

    feat(desempenho-controller): implementa busca específica, atualização e remoção de desempenhos
    Adiciona novas funcionalidades ao módulo de Desempenho:
    
    - Busca de desempenho específico por aluno, mês e ano
    - Atualização de descrição do desempenho por id
    - Remoção de desempenho por id

--- backend/src/modules/desempenho/desempenho.controller.ts
+++ backend/src/modules/desempenho/desempenho.controller.ts
@@ -110,3 +110,153 @@ export const listByStudent = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const getPerformanceByMonth = async (req: Request,res: Response) => {
+  try {
+    const studentId = req.params.studentId as string;
+
+    const month = Number(req.query.month);
+    const year = Number(req.query.year);
+
+    if (!month || !year) {
+      return res.status(400).json({
+        message: "month e year são obrigatórios",
+      });
+    }
+
+    if (!studentId) {
+      return res.status(400).json({
+        message: "studentId não informado",
+      });
+    }
+
+    if (!Types.ObjectId.isValid(studentId)) {
+      return res.status(400).json({
+        message: "studentId inválido",
+      });
+    }
+
+    const aluno = await Aluno.findById(studentId);
+
+    if (!aluno) {
+      return res.status(404).json({
+        message: "Aluno(a) não encontrado(a)",
+      });
+    }
+
+    const desempenhoEspecifico = await Desempenho.findOne({
+      studentId,
+      month,
+      year,
+    });
+
+    if (!desempenhoEspecifico) {
+      return res.status(404).json({
+        message: "Desempenho não encontrado",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Desempenho específico do(a) aluno(a) listado com sucesso",
+      data: desempenhoEspecifico,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Erro interno do servidor",
+    });
+  }
+};
+
+export const updateDesempenho = async (req: Request, res: Response) => {
+    try{
+        const id = req.params.id as string;
+
+        const { description } = req.body;
+
+        if(!id){
+            return res.status(400).json({
+                message: "Id não informado",
+            });
+        }
+
+        if(!Types.ObjectId.isValid(id)){
+            return res.status(400).json({
+                message: "Id inválido",
+            });
+        }
+
+        if (!description) {
+        return res.status(400).json({
+            message: "Descrição não enviada",
+        });
+        }
+
+        const desempenhoUpdate = await Desempenho.findByIdAndUpdate(
+            id,
+            {
+                description,
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if(!desempenhoUpdate ){
+            return res.status(404).json({
+                message: "Desempenho não encontrado",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Desempenho atualizado com sucesso",
+            data: desempenhoUpdate,
+        });
+        
+    }catch(error){
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Erro interno do servidor",
+        });
+    }
+};
+
+export const deleteDesempenhoById = async (req: Request, res: Response) => {
+    try{
+        const id = req.params.id as string;
+
+        if(!id){
+            return res.status(400).json({
+                message: "Id não informado",
+            });
+        }
+
+        if(!Types.ObjectId.isValid(id)){
+            return res.status(400).json({
+                message: "Id inválido",
+            });
+        }
+
+        const desempenho = await Desempenho.findByIdAndDelete(id);
+
+        if(!desempenho){
+            return res.status(404).json({
+                message: "Desempenho não encontrado",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Desempenho deletado com sucesso",
+        });
+    }catch(error){
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Erro interno do servidor",
+        });       
+    }
+}
